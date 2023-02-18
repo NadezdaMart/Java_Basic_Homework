6afb0e975e2206d847d669610a823f32cdd0518c
nadezdamartynkevic <nadezda.martynkevic@gmail.com>
2023-02-18 23:28:50 +0100

    homework

---
+++ src/HomeworkFirst.java
@@ -0,0 +1,214 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HomeworkFirst {
+    public static void main(String[] args) {
+
+//        Homework_1
+//        System.out.println("Я умею выводить строчки");
+//        System.out.println("Я начал программировать");
+
+//       int firstNumber = 67;
+//       int secondNumber = 4;
+//       int thirdNumber = 450;
+//       int fourthNumber = 2;
+//       int fifthNumber = 1234;
+//
+//        System.out.println(firstNumber / secondNumber);
+//        System.out.println(thirdNumber * fourthNumber);
+//        System.out.println(fifthNumber * fifthNumber);
+
+
+//        Homework_2
+//
+//        int number = 5;
+//        int secondNumber = number * 3;
+//        int thirdNumber = secondNumber + 3;
+//        int fourthNumber = thirdNumber / 2;
+//        System.out.println(fourthNumber);
+
+//        int numberOfSchoolboys = 4;
+//        int numberOfApples = 17;
+//        int result = numberOfSchoolboys % numberOfApples;
+//        System.out.println(result);
+
+//        Homework_3
+
+//        long populationOfChina = 1_411_778_724;
+//        int numberOfCountries = 197;
+//        long result = populationOfChina * numberOfCountries;
+//        System.out.println(result);
+
+//        int numberOfHoursPerYear = 365 * 24;
+//        long numberOfSecondsPerYear = numberOfHoursPerYear * 3600;
+//        long felixLifeInSeconds = 3_011_456_567L;
+//        long result = felixLifeInSeconds / numberOfSecondsPerYear;
+//        System.out.println(result);
+
+//        Homework_4
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter random numbers:");
+//        int firstNumber = scanner.nextInt();
+//        int secondNumber = scanner.nextInt();
+//
+//        System.out.println(firstNumber / secondNumber);
+//        System.out.println(firstNumber * secondNumber);
+//        System.out.println(firstNumber + secondNumber);
+//        System.out.println(firstNumber - secondNumber);
+
+//        int number = scanner.nextInt();
+//        String line = Integer.toString(number);
+//        char resultFirstChar = line.charAt(0);
+//        char resultSecondChar = line.charAt(1);
+//        char resultThirdChar = line.charAt(2);
+//        System.out.println(resultFirstChar);
+//        System.out.println(resultSecondChar);
+//        System.out.println(resultThirdChar);
+
+//        Homework_5
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//        if (number == 0) {
+//            System.out.println("ошибок не обнаружено");
+//        } else {
+//            System.out.println("ERROR, ERROR, ERROR");
+//        }
+//    }
+//        int firstRound = scanner.nextInt();
+//        int secondRound = scanner.nextInt();
+//
+//        if (firstRound == 36 && secondRound == 17) {
+//            System.out.println("Jackpot");}
+//            else if (firstRound == 36 || secondRound == 17) {
+//                System.out.println("Small win");}
+//                else {
+//                    System.out.println("Losing");
+//                }
+//
+//            }
+//        }
+
+
+//    Homework_6
+//    Scanner scanner = new Scanner(System.in);
+//    String line = scanner.nextLine();
+//    int lineLength = Integer.valueOf(line);
+//
+//    if (lineLength > 10) {
+//        System.out.println(true);
+//    }
+//    else {
+//        System.out.println(false);
+//    }
+
+//        Scanner scanner = new Scanner(System.in);
+//        String line = scanner.nextLine();
+//        char resultFirstChar = line.charAt(0);
+//        char resultLastChar = line.charAt(line.length()-1);
+//
+//        if (resultFirstChar == resultLastChar) {
+//            System.out.println(true);
+//        }
+//        else {
+//            System.out.println(false);
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        String line = scanner.nextLine();
+//        line = line.replace('A', 'a');
+//        line = line.replace('B', 'A');
+//        line = line.replace('a', 'B');
+//
+//        System.out.println(line);
+
+//       Homework_7
+//        Scanner scanner = new Scanner(System.in);
+//        int randomNumber = scanner.nextInt();
+//        while (randomNumber > 0) {
+//            System.out.println("Я начинаю разбираться с циклами");
+//            randomNumber--;
+//        }
+//        String line = scanner.nextLine();
+//        char firstSymbol = line.charAt(0);
+//        int lineLength = line.length();
+//
+//        while (lineLength > 0) {
+//            System.out.println(firstSymbol);
+//            lineLength--;
+//        }
+
+//        Homework_8
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//
+//        for(int i = 2; i <= number/2; i++)
+//            if(number % i == 0) {
+//                System.out.println(i);
+//                System.out.println(Math.max(i, i));
+//
+//            }
+//        double amount = 1000;
+//        double percentage = 0.10;
+//        int numberOfYears = 2;
+//        double newAmount;
+//
+//        for (int i = 1; i <= numberOfYears; i++);{
+//            amount = (amount + (amount * percentage));
+//            newAmount = (amount + (amount + (amount * percentage)));
+//            System.out.println(amount);
+//            System.out.println(newAmount);
+//        }
+
+//        Homework_9
+
+//        int[] array = new int[100];
+//        for (int i = 0; i <100; i++) {
+//            array[i] = 10;
+//            array[0] = 0;
+//            array[99] = 0;
+//        }
+//        System.out.println(Arrays.toString(array));
+
+//        int[] array = new int[100];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = (37 * i * 3) ;
+//
+//        }
+//
+//        System.out.println(Arrays.toString(array));
+//        int sum = 0;
+//        for (int i = 0; i < array.length; i++)
+//        sum = sum + array[i];
+//        System.out.println(sum);
+
+//        Homework_10
+//        int a = getNumberFromConsole();
+//        int b = getNumberFromConsole();
+//
+//        int sum = Integer.sum(a,b);
+//        int division = Math.floorDiv(a,b);
+//        int multiplying = Math.multiplyExact(a,b);
+//
+//
+//        System.out.println(sum);
+//        System.out.println(division);
+//        System.out.println(multiplying);
+//    }
+//public static int getNumberFromConsole() {
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//        return number;
+
+
+
+
+    }
+
+
+
+
+
+
+}
